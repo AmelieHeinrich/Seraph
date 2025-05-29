b5f7232d9a242a4468af26af814d428b122c0531
Amélie Heinrich <MikuoH15TH@gmail.com>
2025-05-29 11:55:58 +0200

    ext: added vma to build system

---
+++ ThirdParty/Vulkan/Source/Vulkan.cpp
@@ -0,0 +1,7 @@
+//
+// > Notice: Amélie Heinrich @ 2025
+// > Create Time: 2025-05-29 11:42:52
+//
+
+#define VMA_IMPLEMENTATION
+#include <vma/vk_mem_alloc.h>
--- ThirdParty/Vulkan/xmake.lua
+++ ThirdParty/Vulkan/xmake.lua
@@ -4,9 +4,10 @@
 --
 
 target("Vulkan")
-    set_kind("headeronly")
+    set_kind("static")
     set_group("Dependencies")
 
+    add_files("Source/Vulkan.cpp")
     add_includedirs(os.getenv("VULKAN_SDK") .. "/Include", { public = true })
     add_linkdirs(os.getenv("VULKAN_SDK") .. "/Lib", { public = true })
     add_syslinks("volk", { public = true })
